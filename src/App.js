f4e5df4db02eef0e14b77fd9e683603988889a5b
roshinys <roshinchaya@gmail.com>
2023-07-25 19:08:14 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Login from "./component/Auth/Login/Login";
+import Register from "./component/Auth/Register/Register";
+import DashBoard from "./component/DashBoard/DashBoard";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard />,
+  },
+  {
+    path: "/",
+    element: <Navigate to="/dashboard" />,
+  },
+]);
 
 function App() {
-  return <div>Frontend</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
